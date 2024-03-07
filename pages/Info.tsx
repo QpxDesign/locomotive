4a8ab8684df03103edfa02577f49d2c92d49f624
qpxdesign <qpxdesign@users.noreply.github.com>
2024-03-06 19:28:31 -0500

    added IAPs

--- pages/Info.tsx
+++ pages/Info.tsx
@@ -9,7 +9,43 @@ import {
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import {Linking} from 'react-native';
+import {
+  requestPurchase,
+  getProducts,
+  finishTransaction,
+} from 'react-native-iap';
+
 export default function Info() {
+  const productIds = {skus: ['donation4', 'donation5', 'donation6']};
+  purchase = async (sku: string) => {
+    try {
+      await requestPurchase({
+        sku,
+        andDangerouslyFinishTransactionAutomaticallyIOS: true,
+      });
+    } catch (err) {
+      //  alert(err.message);
+    }
+  };
+  async function BuyProductFromId(id: string) {
+    getProducts(productIds)
+      .then(success => {
+        let product = success.find(a => a.productId == id);
+        if (product !== undefined) {
+          purchase(product.productId);
+        } else {
+          alert('Unknown Error');
+        }
+      })
+      .then(ok => {})
+      .catch(error => {
+        //alert(error);
+      })
+      .catch(error => {
+        //alert(error);
+      });
+  }
+
   return (
     <LinearGradient
       colors={['#172C47', '#006CB8']}
@@ -51,6 +87,32 @@ export default function Info() {
               Contact Support
             </Text>
           </Text>
+          <Text style={{color: 'white', fontWeight: 700, fontSize: 24}}>
+            Donate
+          </Text>
+          <Text style={{color: 'white', fontSize: 17}}>
+            Developing and Running AmTrack takes a significant amount of time,
+            effort, and money. To keep AmTrack Free for all, please consider
+            donating below. Any amount is greatly appreciated
+          </Text>
+          <View
+            style={{flexDirection: 'row', justifyContent: 'center', gap: 40}}>
+            <Text
+              style={{color: '#fdba74', fontSize: 27, fontWeight: 'bold'}}
+              onPress={() => BuyProductFromId('donation4')}>
+              $0.99
+            </Text>
+            <Text
+              style={{color: '#fdba74', fontSize: 27, fontWeight: 'bold'}}
+              onPress={() => BuyProductFromId('donation5')}>
+              $1.99
+            </Text>
+            <Text
+              style={{color: '#fdba74', fontSize: 27, fontWeight: 'bold'}}
+              onPress={() => BuyProductFromId('donation6')}>
+              $4.99
+            </Text>
+          </View>
         </ScrollView>
       </SafeAreaView>
     </LinearGradient>
